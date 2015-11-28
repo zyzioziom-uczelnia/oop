265df8f6136dcc83646414df2fbe3aeff1fca1e8
Patryk Kalinowski <patryk.kalinowski@netguru.pl>
2015-11-28 14:06:21 +0100

    working interface

--- Animal.php
+++ Animal.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once ('AnimalInterface.php');
+
 class Animal implements AnimalInterface {
   public $species;
   public $sex;
@@ -15,6 +18,36 @@ class Animal implements AnimalInterface {
   public function thisAnimal() {
     return "Hello, this is me - your ". $this->species .". I am ". $this->color ." and I do ". $this->sound ."!\n";
   }
+
+  public function getSpecies() {
+    return $this->species;
+  }
+  public function setSpecies($species) {
+    $this->species = $species;
+    return $this->species;
+  }
+  public function getSex() {
+    return $this->sex;
+  }
+  public function setSex($sex) {
+    $this->sex = $sex;
+    return $this->sex;
+  }
+  public function getSound() {
+    return $this->sound;
+  }
+  public function setSound($sound) {
+    $this->sound = $sound;
+    return $this->sound;
+  }
+  public function getColor() {
+    return $this->color;
+  }
+  public function setColor($color) {
+    $this->color = $color;
+    return $this->color;
+  }
+
 }
 
 
--- AnimalInterface.php
+++ AnimalInterface.php
@@ -2,34 +2,14 @@
 
 interface AnimalInterface {
 
-  public function getSpecies() {
-    return $this->species;
-  }
-  public function setSpecies($species) {
-    $this->species = $species;
-    return $this->species;
-  }
-  public function getSex() {
-    return $this->sex;
-  }
-  public function setSex($sex) {
-    $this->sex = $sex;
-    return $this->sex;
-  }
-  public function getSound() {
-    return $this->sound;
-  }
-  public function setSound($sound) {
-    $this->sound = $sound;
-    return $this->sound;
-  }
-  public function getColor() {
-    return $this->color;
-  }
-  public function setColor($color) {
-    $this->color = $color
-    return $this->color;
-  }
+  public function getSpecies();
+  public function setSpecies($species);
+  public function getSex();
+  public function setSex($sex);
+  public function getSound();
+  public function setSound($sound);
+  public function getColor();
+  public function setColor($color);
 
 
 
